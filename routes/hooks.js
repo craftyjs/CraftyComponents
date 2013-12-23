8025597d2dcd666c53978138331e7698e4b0a65e
Giovanni Cappellotto <potomak84@gmail.com>
2013-12-23 23:49:52 +0100

    Retrieve file contents from github

--- routes/hooks.js
+++ routes/hooks.js
@@ -1,4 +1,17 @@
+var URL = require('url');
+var githubContents = require('github-contents');
+
 // POST /hooks/github
-exports.github = function(req, res){
-  res.json(200);
+exports.github = function(req, res, next) {
+  var payload = JSON.parse(req.body.payload);
+  var repoUrl = URL.parse(payload.repository.url);
+
+  githubContents(repoUrl.path.substring(1), 'asd.json', function(err, contents) {
+    if (err) {
+      return next(err);
+    }
+
+    console.log(contents);
+    res.json(200);
+  });
 };
