50e53e49b239d28401fd96629ededd4b821fe565
Giovanni Cappellotto <potomak84@gmail.com>
2013-12-24 16:37:56 +0100

    Update root and components#index templates

--- routes/components.js
+++ routes/components.js
@@ -1,4 +1,9 @@
+var Component = require('../models/component');
+
 // GET /components
-exports.index = function(req, res) {
-  res.render('components/index', { title: 'Components' });
+exports.index = function(req, res, next) {
+  Component.find(function(err, components) {
+    if (err) return next(err);
+    res.render('components/index', { components: components });
+  });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
+var Component = require('../models/component');
+
 // GET /
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res, next) {
+  Component.find(function(err, components) {
+    if (err) return next(err);
+    res.render('index', { components: components });
+  });
 };
